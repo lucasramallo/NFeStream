17b5304ecde3d37a584cc3fa7bd0db65b5000d76
lucasramallo <lucasdebrito.jd@gmail.com>
2024-09-08 20:16:50 -0300

    first commit

---
+++ src/main/java/com/github/lucasramallo/nfestream/NfestreamApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasramallo.nfestream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfestreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfestreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasramallo/nfestream/NfestreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasramallo.nfestream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfestreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
