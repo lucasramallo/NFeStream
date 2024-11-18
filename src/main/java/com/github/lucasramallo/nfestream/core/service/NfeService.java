49651b33cd591a80318ae9d7cb42cf561e12ac6a
lucasramallo <lucasdebrito.jd@gmail.com>
2024-11-17 21:32:44 -0300

    jaxbToImpostoEntity

--- src/main/java/com/github/lucasramallo/nfestream/core/service/NfeService.java
+++ src/main/java/com/github/lucasramallo/nfestream/core/service/NfeService.java
@@ -18,6 +18,7 @@ public class NfeService {
         }
 
         NfeJAXBModel nfeModel = convertXmlToModel(inputStream);
+        
         ArrayList<Imposto> impostos = JaxbToImposto.execute(nfeModel.getInfNFe().getDet().getImposto());
     }
 
